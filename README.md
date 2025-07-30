38306e7384835f86798b0fab88142745b966d85c
Chamal1120 <kenway213beep@gmail.com>
2025-07-30 20:23:54 +0530

    add raw mode toggle funcs

---
+++ README.md
@@ -0,0 +1,3 @@
+## This is a simple implementation of the kilo text editor using the [Build Your Own Text Editor](https://viewsourcecode.org/snaptoken/kilo/) guide.
+
+
--- kilo.c
+++ kilo.c
@@ -1,8 +1,34 @@
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
-int main() {
-  char c;
+// Store the original terminal attribute in a
+// global variable
+struct termios orig_termios;
 
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+
+  // atexit comes from stdlib.h
+  // This allows to register disableRawMode function to
+  // be called automatically when the program exits
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  // Turn off ECHO makes each typed printed to the terminal
+  raw.c_lflag &= ~(ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+  enableRawMode();
+
+  char c;
   // Put the terminal to canonical mode
   // reads until user presses the key 'q'
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
