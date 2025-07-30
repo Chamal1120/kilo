c7eb49cf324667420c8ce1b79eb6417f9710c55f
Chamal1120 <kenway213beep@gmail.com>
2025-07-30 19:43:45 +0530

    add one more comment

--- kilo.c
+++ kilo.c
@@ -3,7 +3,8 @@
 int main() {
   char c;
 
-  // Put the terminal to canonical mode 
+  // Put the terminal to canonical mode
+  // reads until user presses the key 'q'
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
 }
